d0e365bd10e43e332cb83ca83928ececb883e8d7
zoltan-santha <zoltan.santha.physics@gmail.com>
2026-08-05 15:57:13 +0200

    v0.2.0

--- src/pysideband/__init__.py
+++ src/pysideband/__init__.py
@@ -1,3 +1,3 @@
-__version__ = "0.1.2"
+__version__ = "0.2.0"
 __description__ = "pysideband software for calculating spectral-function lineshapes from ab initio calculations via phonon interpolation"
 __software_panel_message__ = "[TODO]"
